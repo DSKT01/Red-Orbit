348b60cba1818f192ff740206f59dcb8db6123b4
DSKT01 <diego99gu@hotmail.com>
2017-09-27 22:23:48 -0500

    V.0.0
    Creacion del proyecto

---
+++ Red Orbit/Assets/Codigo/ControlJugador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour {
+    Movimiento mMovimiento;
+	// Use this for initialization
+	void Start () {
+        mMovimiento = GetComponent<Movimiento>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mMovimiento.HMove();
+	}
+}
---
+++ Red Orbit/Assets/Codigo/Movimiento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    [SerializeField]
+    float mag;
+    [SerializeField]
+    float magR;
+    Transform mTransform;
+
+	// Use this for initialization
+	void Start () {
+        mTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HMove()
+    {
+        Vector3 dirV = transform.forward;
+        float senV = Input.GetAxis("Vertical");
+        Vector3 VelocidadV = senV * dirV * mag;
+        mTransform.position += VelocidadV * Time.deltaTime;
+
+        Vector3 dirH = transform.up;
+        float senH = Input.GetAxis("Horizontal");
+        Vector3 VelocidadH = senH * dirH;
+        mTransform.position += VelocidadH * Time.deltaTime;
+
+
+
+    }
+}
