9bd7fea38da03db2848c32c9a55e9c3a89bcb526
DSKT01 <diego99gu@hotmail.com>
2017-09-30 16:33:48 -0500

    V.0.4

--- Red Orbit/Assets/Codigo/MovCamara.cs
+++ Red Orbit/Assets/Codigo/MovCamara.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovCamara : MonoBehaviour {
+public class MovCamara : MonoBehaviour
+{
     Transform mTrans;
     Transform jTrans;
     public float magnitud = 6;
 
-	void Start () {
+    void Start()
+    {
         mTrans = GetComponent<Transform>();
         //cambiar "mona" por player
         jTrans = GameObject.Find("mona").GetComponent<Transform>();
-	}
-	
-	
-	void Update () {
+    }
 
-        float distancia = Vector3.Distance(mTrans.position,jTrans.position)/(1.1f);
+
+    void Update()
+    {
+
+        float distancia = Vector3.Distance(mTrans.position, jTrans.position) / (1.1f);
         Ray Rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 ubicacionM = Rayo.GetPoint(distancia);
-        Vector3 puntero = new Vector3(ubicacionM.x,mTrans.position.y, ubicacionM.z);
-
+        Vector3 puntero = new Vector3(ubicacionM.x, mTrans.position.y, ubicacionM.z);
         Vector3 posi = (puntero + jTrans.position) / 2;
-
-        mTrans.position = new Vector3 (posi.x,12,posi.z-4) ;
+        Vector3 posiF = new Vector3(posi.x, 13, posi.z - 5);
+        float x = ((Vector3.Distance(mTrans.position, posiF)) / 2) * 30f * Time.deltaTime;
+        mTrans.position = Vector3.MoveTowards(mTrans.position, posiF, x);
     }
 }
